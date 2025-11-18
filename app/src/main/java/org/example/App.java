50da4537b1b799b3a7dbc3c65a265a1c7eafef05
Juunibyou <danijunn@gmail.com>
2025-11-18 01:56:12 -0600

    update

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,9 +1,16 @@
 package org.example;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
     
+      int[] n = {10, 50, 100, 500, 1000, 5000, 10000};
+      double[] p = {0.25, 0.5, 0.75};
+      int m = 10;
 
-      
+      Random random = new Random();
+
+
+
     }
 }
