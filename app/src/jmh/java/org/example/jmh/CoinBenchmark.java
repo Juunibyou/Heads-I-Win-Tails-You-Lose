5128b78a59e3439c3bcf364bd50b6ebc3c861449
Juunibyou <danijunn@gmail.com>
2025-11-18 13:35:04 -0600

    benchmark

---
+++ app/src/jmh/java/org/example/jmh/CoinBenchmark.java
@@ -0,0 +1,45 @@
+package org.example.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Random;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1, warmups = 2)
+@Warmup(iterations = 2)
+
+public class CoinBenchmark {
+
+  int[] n = {100, 500, 1000, 5000, 10000, 50000};
+  double[] p = {0.25, 0.5, 0.75};
+  int m = 10;
+  Random random;
+
+  @Setup
+  public void setup() {
+    random = new Random();
+  }
+
+  @Benchmark
+  @Timeout(time = 5, timeUnit = TimeUnit.SECONDS)
+  public void flipCoinBenchmark(Blackhole bh) {
+    for (double prob : p) {
+      for(int flips : n) {
+        for(int trial = 1; trial <= m; trial++) {
+          int headsCount = 0;
+          for (int i = 0; i < flips; i++) {
+            if (random.nextDouble() < prob) {
+              headsCount++;
+            }
+          }
+          bh.consume(headsCount);
+        }
+      }
+    }
+  }
+}
